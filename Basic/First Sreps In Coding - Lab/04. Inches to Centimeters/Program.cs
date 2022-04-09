c87c4749cdb16b88b6128ed600d04fd1299ce0ed
Buzzii9 <ebazenikovich@gmail.com>
2022-04-09 12:46:33 +0300

    here is my first ever exercises

---
+++ Basic/First Sreps In Coding - Lab/04. Inches to Centimeters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inches");
+
+            var inches = double.Parse(Console.ReadLine());
+
+            var centimeters = inches * 2.54;
+
+            Console.WriteLine("Centimeters");
+
+            Console.WriteLine(centimeters);
+
+
+        }
+    }
+}
---
+++ Basic/First Sreps In Coding - Lab/08. Pet Shop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dogFoodQuantity = double.Parse(Console.ReadLine());
+            double notDogFoodQuantity = double.Parse(Console.ReadLine());
+            double dogFoodSum = dogFoodQuantity * 2.50;
+            double notDogFoodSum = notDogFoodQuantity * 4.00;
+            double allFoodSum = dogFoodSum + notDogFoodSum;
+
+            Console.WriteLine($"{allFoodSum} lv.");
+
+        }
+    }
+}
