f1638b8cba9b624cae1864f26cdfb3440a94cc9e
Buzzii9 <ebazenikovich@gmail.com>
2022-04-15 19:03:02 +0300

    this is my exercises for Conditional Statements

---
+++ Basic/Conditional Statements - Lab/07. Area of Figures/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _07._Area_of_Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inp = Console.ReadLine();
+            double area = 0;
+
+            if (inp == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                area = a * a;
+            }
+
+            else if (inp == "rectangle")
+            {
+                double w = double.Parse(Console.ReadLine());
+                double h = double.Parse(Console.ReadLine());
+                area = w * h;
+            }
+
+            else if (inp == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                area = Math.PI * r * r;
+            }
+
+            else if (inp == "triangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                area = a * b / 2;
+            }
+
+            Console.WriteLine($"{area:F3}");
+        }
+    }
+}
