0efd6e0690102dcda69c52e8c3c9cba4bc40b132
Buzzii9 <ebazenikovich@gmail.com>
2022-04-24 15:49:50 +0300

    switch,case exercises

---
+++ Basic/Conditional Statements Advanced - Exercise/01. Cinema/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01._Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Четем от конзолата
+            string ticketType = Console.ReadLine();
+            int row = int.Parse(Console.ReadLine());
+            int col = int.Parse(Console.ReadLine());
+            // izchislqvame
+            double income = row * col;
+            //проверяваме типа на билетите
+            if (ticketType == "Premiere")
+            {
+                income = income * 12.00;
+            }
+            else if (ticketType == "Normal")
+            {
+                income = income * 7.50;
+            }
+            else
+            {
+                income = income * 5.00;
+            }
+
+            //извеждаме резултат
+            Console.WriteLine("{0:f2} leva", income);
+        }
+    }
+}
---
+++ Basic/Conditional Statements Advanced - Exercise/09. Ski Trip/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace _09._Ski_Trip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double days = double.Parse(Console.ReadLine());
+            string room = Console.ReadLine();
+            string e = Console.ReadLine();
+            double price = 0.0;
+            double totalprice = 0.0;
+            double totalprice2 = 0.0;
+            if (room == "room for one person")
+            {
+                price = 18.00;
+                if (e == "positive")
+                {
+                    totalprice = (days - 1) * price + (days - 1) * price * 0.25;
+                    Console.WriteLine("{0:F2}", totalprice);
+                }
+
+                else if (e == "negative")
+
+
+                {
+                    totalprice = (days - 1) * price - (days - 1) * price * 0.10;
+                    Console.WriteLine("{0:F2}", totalprice);
+                }
+
+            }
+            else if (room == "apartment")
+            {
+                price = 25.00;
+                if (e == "positive")
+                {
+                    if (days < 10)
+                    {
+                        totalprice2 = (days - 1) * price; totalprice = totalprice2 - totalprice2 * 0.30;
+                        Console.WriteLine("{0:F2}", (totalprice + totalprice * 0.25));
+                    }
+                    else if (days >= 10 && days <= 15)
+                    {
+                        totalprice2 = (days - 1) * price; totalprice = totalprice2 - totalprice2 * 0.35;
+                        Console.WriteLine("{0:F2}", (totalprice + totalprice * 0.25));
+                    }
+                    else if (days > 15)
+                    {
+                        totalprice2 = (days - 1) * price; totalprice = totalprice2 - totalprice2 * 0.50;
+                        Console.WriteLine("{0:F2}", (totalprice + totalprice * 0.25));
+                    }
+                }
+                else if (e == "negative")
+                {
+
+                    if (days < 10)
+                    {
+                        totalprice2 = (days - 1) * price; totalprice = totalprice2 - totalprice2 * 0.30;
+                        Console.WriteLine("{0:F2}", (totalprice - totalprice * 0.10));
+                    }
+                    else if (days >= 10 && days <= 15)
+                    {
+                        totalprice2 = (days - 1) * price; totalprice = totalprice2 - totalprice2 * 0.35;
+                        Console.WriteLine("{0:F2}", (totalprice - totalprice * 0.10));
+                    }
+                    else if (days > 15)
+                    {
+                        totalprice2 = (days - 1) * price; totalprice = totalprice2 - totalprice2 * 0.50;
+                        Console.WriteLine("{0:F2}", (totalprice - totalprice * 0.10));
+                    }
+                }
+
+            }
+
+            else if (room == "president apartment")
+            {
+                price = 35.00;
+                if (e == "positive")
+                {
+                    if (days < 10)
+                    {
+                        totalprice2 = (days - 1) * price; totalprice = totalprice2 - totalprice2 * 0.10;
+                        Console.WriteLine("{0:F2}", (totalprice + totalprice * 0.25));
+                    }
+                    else if (days >= 10 && days <= 15)
+                    {
+                        totalprice2 = (days - 1) * price; totalprice = totalprice2 - totalprice2 * 0.15;
+                        Console.WriteLine("{0:F2}", (totalprice + totalprice * 0.25));
+                    }
+                    else if (days > 15)
+                    {
+                        totalprice2 = (days - 1) * price; totalprice = totalprice2 - totalprice2 * 0.20;
+                        Console.WriteLine("{0:F2}", (totalprice + totalprice * 0.25));
+                    }
+                }
+                else if (e == "negative")
+                {
+                    if (days < 10)
+                    {
+                        totalprice2 = (days - 1) * price; totalprice = totalprice2 - totalprice2 * 0.10;
+                        Console.WriteLine("{0:F2}", (totalprice - totalprice * 0.10));
+                    }
+                    else if (days >= 10 && days <= 15)
+                    {
+                        totalprice2 = (days - 1) * price; totalprice = totalprice2 - totalprice2 * 0.15;
+                        Console.WriteLine("{0:F2}", (totalprice - totalprice * 0.10));
+                    }
+                    else if (days > 15)
+                    {
+                        totalprice2 = (days - 1) * price; totalprice = totalprice2 - totalprice2 * 0.20;
+                        Console.WriteLine("{0:F2}", (totalprice - totalprice * 0.10));
+                    }
+                }
+            }
+        }
+    }
+}
