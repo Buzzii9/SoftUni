7dd543e283b38e357c8bcf453c74d87da4491620
Buzzii9 <ebazenikovich@gmail.com>
2022-05-05 21:27:24 +0300

    for loops
    my simple for loop exercises

---
+++ Basic/For Loop - Lab/06. Vowels Sum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _06._Vowels_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum1 = 0;
+            int sum2 = 0;
+            int sum3 = 0;
+            int sum4 = 0;
+            int sum5 = 0;
+            string s = Console.ReadLine().ToLower();
+            for (int i = 0; i <= s.Length - 1; i++)
+            {
+                if (s[i] == 'a')
+                    sum1++;
+                else if (s[i] == 'e')
+                    sum2 += 2;
+                else if (s[i] == 'i')
+                    sum3 += 3;
+                else if (s[i] == 'o')
+                    sum4 += 4;
+                else if (s[i] == 'u')
+                    sum5 += 5;
+            }
+            int ResultSum = sum1 + sum2 + sum3 + sum4 + sum5;
+            Console.WriteLine(ResultSum);
+        }
+    }
+}
---
+++ Basic/For Loop - Lab/09. Left and Right Sum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _09._Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sumLeft = 0;
+            int sumRight = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentNumber = int.Parse(Console.ReadLine());
+                sumLeft = sumLeft + currentNumber;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                var currentNumber = int.Parse(Console.ReadLine());
+                sumRight = sumRight + currentNumber;
+            }
+            if (sumLeft == sumRight)
+
+            {
+                Console.WriteLine("Yes, sum = {0}", sumRight);
+            }
+            else
+            {
+                var result = Math.Abs(sumLeft - sumRight);
+                Console.WriteLine("No, diff = {0}", result);
+            }
+        }
+    }
+}
