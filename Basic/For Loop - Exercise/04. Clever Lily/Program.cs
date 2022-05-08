e5216d5e45a9ef4a1f3f8ed55199696240065fc6
Buzzii9 <ebazenikovich@gmail.com>
2022-05-08 17:03:05 +0300

    for loop harder exercises

---
+++ Basic/For Loop - Exercise/04. Clever Lily/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _04._Clever_Lily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int old = int.Parse(Console.ReadLine());
+            double priceForMachine = double.Parse(Console.ReadLine());
+            double priceForToy = double.Parse(Console.ReadLine());
+
+            double toy = 0;
+            double savedMoneyFromBirthDay = 0.0;
+
+            double totalMoney;
+            double secondMoney = 0;
+            double counter = 0;
+            double priceFromToy = 0;
+
+            for (int i = 1; i <= old; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    savedMoneyFromBirthDay = savedMoneyFromBirthDay + 10;
+                    secondMoney = secondMoney + savedMoneyFromBirthDay;
+                    counter++;
+                }
+                else
+                    toy++;
+
+            }
+            priceFromToy = toy * priceForToy;
+
+            totalMoney = secondMoney + priceFromToy - counter;
+
+            if (totalMoney >= priceForMachine)
+            {
+                Console.WriteLine("Yes! {0:f2}", Math.Abs(totalMoney - priceForMachine));
+            }
+            else
+            {
+                Console.WriteLine("No! {0:F2}", Math.Abs(priceForMachine - totalMoney));
+            }
+             
+        }
+    }
+}
