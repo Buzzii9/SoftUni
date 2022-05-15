1df4d85c40160ea70ffe31fe392ecf3791a02007
Buzzii9 <ebazenikovich@gmail.com>
2022-05-15 22:19:30 +0300

    main while loop exercises

---
+++ Basic/While Loop - Lab/06. Max Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06._Max_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.MinValue;
+            string input = Console.ReadLine();  
+
+            while (input != "Stop")
+            {
+                int n = int.Parse(input);
+
+                if (n > number)
+                {
+                    number = n;
+                }
+
+                input = Console.ReadLine(); 
+            }
+
+
+            Console.WriteLine(number);
+        }
+    }
+}
---
+++ Basic/While Loop - Lab/07. Min Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07._Min_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int maxmin = int.MaxValue;
+
+            while (input != "Stop")
+            {
+                int num = int.Parse(input);
+                if (num < maxmin)
+                {
+                    maxmin = num;
+                }
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(maxmin);
+        }
+    }
+}
