c562bb256db838556588c54de0734ed2091ae9c5
Buzzii9 <ebazenikovich@gmail.com>
2022-05-17 23:21:10 +0300

    nested loops
    these are my exercises for nested loops

---
+++ Basic/Nested Loops - Lab/06. Building/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _06._Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxFloors = int.Parse(Console.ReadLine());
+            int maxRooms = int.Parse(Console.ReadLine());
+
+            for (int floor = maxFloors; floor > 0; floor--)
+            {
+                string floorString = "";
+                for (int room = 0; room < maxRooms; room++)
+                {
+                    if (floor == maxFloors)
+                    {
+                        floorString += "L" + floor + room + " ";
+                    }
+                    else if (floor % 2 == 0)
+                    {
+                        floorString += "O" + floor + room + " ";
+                    }
+                    else
+                    {
+                        floorString += "A" + floor + room + " ";
+                    }
+                }
+                Console.WriteLine(floorString);
+            }
+            
+        }
+    }
+}
